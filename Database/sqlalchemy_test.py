42db5213770d5fcb70a9ba20d6758c69137bde03
Erikalizth27 <31712449+Erikalizth27@users.noreply.github.com>
2019-11-30 21:23:47 -0500

    Feature: Added Creating Session

---
+++ Database/sqlalchemy_test.py
@@ -0,0 +1,54 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from pprint import pprint
+
+# Create an engine that stores data in the local directory's
+# sqlalchemy_example.db file.
+engine = create_engine('sqlite:////web/Sqlite-Data/example.db')
+
+# this loads the sqlalchemy base class
+Base = declarative_base()
+
+
+# Setting up the classes that create the record objects and define the schema
+
+class Person(Base):
+    __tablename__ = 'person'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Address(Base):
+    __tablename__ = 'address'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    street_name = Column(String(250))
+    street_number = Column(String(250))
+    post_code = Column(String(250), nullable=False)
+    # creates the field to store the person id
+    person_id = Column(Integer, ForeignKey('person.id'))
+    # creates the relationship between the person and addresses.  backref adds a property to the Person class to retrieve addresses
+    person = relationship("Person", backref="addresses")
+
+
+# Create all tables in the engine. This is equivalent to "Create Table"
+# statements in raw SQL.
+Base.metadata.create_all(engine)
+
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
