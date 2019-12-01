64397afae4ffc1bfe3a793428a3be0f571b697d3
Erikalizth27 <31712449+Erikalizth27@users.noreply.github.com>
2019-11-30 21:30:42 -0500

    Feature: Added Querying Data

--- Database/sqlalchemy_test.py
+++ Database/sqlalchemy_test.py
@@ -74,3 +74,19 @@ DBSession = sessionmaker(bind=engine)
 # revert all of them back to the last commit by calling
 # session.rollback()
 session = DBSession()
+# joins Person on Address
+all_people = session.query(Person).join(Address).all()
+
+# Accessing a person with their address, You have to loop the addresses property and remember it was added by the
+# backref on the addresses class
+for person in all_people:
+    # use the __dict__ magic method to have the object print it's properties
+    pprint(person.__dict__)
+    for address in person.addresses:
+        pprint(address.__dict__)
+
+# Retrieving the inverse of the relationship.  Notice I reverse the Person and Address to load the Address table
+all_addresses = session.query(Address).join(Person).all()
+for address in all_addresses:
+    # showing how to use the print function with printing text and data at the same time easily
+    print(f'{address.person.name} has a postal code of {address.post_code}')
